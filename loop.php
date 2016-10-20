98a77000ef91dd4e06994037b76ec82ab479296a
Igoe Creative <goweb@goigoecreative.com>
2016-10-20 16:54:43 -0400

    Comment Out Change
    Description of Thumbnail Function change.

--- loop.php
+++ loop.php
@@ -4,7 +4,7 @@
 
 		<?php if ( has_post_thumbnail()) :?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+				<?php the_post_thumbnail(array(120,120)); // Declare img size in pixels you need inside the array ?>
 			</a>
 		<?php endif; ?>
 
