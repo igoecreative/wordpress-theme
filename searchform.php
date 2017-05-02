bb7cc26a25c9d1e2c1edf039432313da32a40877
Igoe Creative <goweb@goigoecreative.com>
2017-05-02 15:43:27 -0400

    Update to searchform.php Query

--- searchform.php
+++ searchform.php
@@ -1,9 +1,11 @@
 <!-- search form to copy and paste into doc -->
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-<div>
-<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+<div id="search">
+	<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<div>
+			<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+			<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+			<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+		</div>
+	</form>
 </div>
-</form>
 <!-- /search -->
