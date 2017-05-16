0d1ada12c8c72a3cef39c2773770e09b3e243a69
Igoe Creative <goweb@goigoecreative.com>
2017-05-16 12:49:26 -0400

    Version 2.0 Updates

--- archive.php
+++ archive.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<section>
-			<h1><?php _e( 'Archives', 'igoecreative' ); ?></h1>
-			<?php get_template_part('loop'); ?>
-			<?php get_template_part('pagination'); ?>
-		</section>
-	</main>
+<div class="container">
+	<div class="inner default">
+		<div class="row">
+			<div class="col-xs-12">
+
+				<h1><?php _e( 'Archives', 'igoecreative' ); ?></h1>
+				<?php get_template_part('loop'); ?>
+				<?php get_template_part('pagination'); ?>
+
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
