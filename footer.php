9e3ebd77dd3e7657703b421d06d797bf073753ba
Igoe Creative <goweb@goigoecreative.com>
2017-05-16 12:54:28 -0400

    Version 2.0 Updates

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 			<footer class="footer" role="contentinfo">
 				<div class="container">
 					<div class="row">
-						<div class="copyright">
-							&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
-							<a href="https://goigoecreative.com" target="_blank" title="Igoe Creative">Powered by Igoe Creative</a>.
+						<div class="col-xs-12">
+							<div class="copyright">
+								&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
+								<a href="https://goigoecreative.com" target="_blank" title="Igoe Creative">Powered by Igoe Creative</a>.
+							</div>
 						</div>
 					</div>
 				</div>
