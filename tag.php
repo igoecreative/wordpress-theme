eec0d964cdb3b6d85426e7427b8b16d65acb6348
Igoe Creative <goweb@goigoecreative.com>
2017-05-16 12:58:11 -0400

    Version 2.0 Updates

--- tag.php
+++ tag.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<section>
-			<h1><?php _e( 'Tag Archive: ', 'igoecreative' ); echo single_tag_title('', false); ?></h1>
-			<?php get_template_part('loop', 'igoecreative' ); ?>
-			<?php get_template_part('pagination', 'igoecreative' ); ?>
-		</section>
-	</main>
+<div class="container">
+	<div class="inner default">
+		<div class="row">
+			<div class="col-xs-12">
+
+				<h1><?php _e( 'Tag Archive: ', 'igoecreative' ); echo single_tag_title('', false); ?></h1>
+				<?php get_template_part('loop', 'igoecreative' ); ?>
+				<?php get_template_part('pagination', 'igoecreative' ); ?>
+
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
