6845879527f42d5a38bec877223d1fd24ca721dd
Igoe Creative <goweb@goigoecreative.com>
2017-10-26 16:08:07 -0400

    Update Class Structure

--- search.php
+++ search.php
@@ -1,11 +1,15 @@
 <?php get_header(); ?>
 
 	<div class="container">
-		<section>
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'igoecreative' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
-			<?php get_template_part('loop'); ?>
-			<?php get_template_part('pagination'); ?>
-		</section>
+		<div class="inner">
+			<div class="row">
+				<section class="col-xs-12">
+					<h1><?php echo sprintf( __( '%s Search Results for ', 'igoecreative' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+					<?php get_template_part('loop'); ?>
+					<?php get_template_part('pagination'); ?>
+				</section>
+			</div>
+		</div>
 	</div>
 
 <?php get_sidebar(); ?>
