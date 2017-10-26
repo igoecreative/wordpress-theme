acf365e1ad3af4ded25b2d963dd5fef2f451b72b
Igoe Creative <goweb@goigoecreative.com>
2017-10-26 16:02:43 -0400

    Remove 2nd Featured Image

--- author.php
+++ author.php
@@ -15,11 +15,6 @@
 				<?php rewind_posts(); while (have_posts()) : the_post(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<?php if ( has_post_thumbnail()) : ?>
-							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-								<?php the_post_thumbnail(array(120,120)); ?>
-							</a>
-						<?php endif; ?>
 
 						<h2>
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
