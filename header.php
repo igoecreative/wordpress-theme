93273f34841c21da0ce59ba6b78c7bcd6730358b
Igoe Creative <goweb@goigoecreative.com>
2017-10-26 16:00:27 -0400

    Update to Title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php bloginfo('name'); ?></title>
+		<title><?php wp_title(''); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/fav.png" rel="shortcut icon">
