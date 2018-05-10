cbd80230fc26c434fcbcb12badf2133b90f74ada
Igoe Creative <goweb@goigoecreative.com>
2018-05-10 10:32:31 -0400

    Add Gravatar as Login Logo

--- functions.php
+++ functions.php
@@ -268,6 +268,17 @@ function igoeCustomComments($comment, $args, $depth)
 	<?php endif; ?>
 <?php }
 
+function my_login_logo_one() {  ?>
+  <style type="text/css">
+    body.login div#login h1 a {
+      background-image: url(http://leadershipeast.com/wordpress/wp-content/themes/leadership-east/img/gravatar.png);
+      padding-bottom: 30px;
+    }
+  </style>
+<?php }
+
+add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
+
 /***********************************\
   Actions + Filters + ShortCodes
 \***********************************/
